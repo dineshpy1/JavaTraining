ba3f8c10840415e5bf689a421c6eec2b08e35827
dineshpy1 <128726549+dineshpy1@users.noreply.github.com>
2023-06-01 15:45:31 -0500

    Add files via upload
    Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Dinesh Reddy");
+        String name = student.getName();
+        System.out.println("Student's name: " + name);
+    }
+}
